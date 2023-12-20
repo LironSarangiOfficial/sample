15e5af14f0db66b020b74a83791fc8d2b20e9a54
Liron <sarangi.liron7@gmail.com>
2023-12-20 13:59:22 +0530

    Initial Commit

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define vpii vector<pii>
+#define f(i, a, b) for (auto i = a; i < b; ++i)
+#define fd(i, a, b) for (auto i = a; i >= b; --i)
+#define cnl(c) cout << c << "\n";
+
+void solve()
+{
+    cout << "Hello World!" << endl;
+}
+
+int main()
+{
+    int tc=1;
+    cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+    return 0;
+}
